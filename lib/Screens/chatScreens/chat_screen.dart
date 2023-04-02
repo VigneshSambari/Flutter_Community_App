93c74bc5418c0aed08dc6959630ea4a64837c4ca
VickySam1901 <72963230+VickySam1901@users.noreply.github.com>
2023-04-03 00:25:08 +0530

    adding chat clips

--- lib/Screens/chatScreens/chat_screen.dart
+++ lib/Screens/chatScreens/chat_screen.dart
@@ -39,127 +39,128 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-        body: Container(
-      width: size.width,
-      height: size.height,
-      child: Stack(
-        children: [
-          SingleChildScrollView(
-            physics: NeverScrollableScrollPhysics(),
-            child: Column(
-              children: [
-                ListTile(
-                  tileColor: kPrimaryColor,
-                  title: Text(
-                    "Room Name",
-                    style: TextStyle(
-                      fontSize: 18,
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            SingleChildScrollView(
+              physics: NeverScrollableScrollPhysics(),
+              child: Column(
+                children: [
+                  ListTile(
+                    tileColor: kPrimaryColor,
+                    title: Text(
+                      "Room Name",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "Room description...",
+                      style: TextStyle(
+                        color: kPrimaryLightColor,
+                      ),
+                    ),
+                    leading: Wrap(
+                      children: [
+                        BackButtonNav(),
+                        CircleNetworkPicture(),
+                      ],
+                    ),
+                    trailing: Icon(
+                      Icons.more_vert_rounded,
                       color: Colors.white,
-                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  subtitle: Text(
-                    "Room description...",
-                    style: TextStyle(
-                      color: kPrimaryLightColor,
-                    ),
-                  ),
-                  leading: Wrap(
+                  Stack(
                     children: [
-                      BackButtonNav(),
-                      CircleNetworkPicture(),
+                      Container(
+                        height: size.height,
+                        child: ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: 100,
+                          itemBuilder: (context, index) {
+                            return Text("Hello");
+                          },
+                        ),
+                      ),
+                      SwipeDownRow(events: events),
                     ],
                   ),
-                  trailing: Icon(
-                    Icons.more_vert_rounded,
-                    color: Colors.white,
-                  ),
-                ),
-                Stack(
-                  children: [
-                    Container(
-                      height: size.height,
-                      child: ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: 100,
-                        itemBuilder: (context, index) {
-                          return Text("Hello");
-                        },
-                      ),
-                    ),
-                    SwipeDownRow(events: events),
-                  ],
-                ),
-              ],
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-              width: size.width,
-              decoration: BoxDecoration(
-                color: kPrimaryColor.withOpacity(0.85),
-              ),
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: kPrimaryLightColor,
-                        borderRadius: BorderRadius.circular(15),
-                      ),
-                      child: Row(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.only(left: 10),
-                            child: Icon(
-                              Icons.emoji_emotions_rounded,
-                              color: kPrimaryColor.withOpacity(0.85),
-                            ),
-                          ),
-                          Expanded(
-                            child: TextField(
-                              controller: messageController,
-                              maxLines: null,
-                              decoration: InputDecoration(
-                                contentPadding: EdgeInsets.symmetric(
-                                  horizontal: 10,
-                                ),
-                                border: InputBorder.none,
-                                hintText: "Type here...",
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(right: 10),
-                            child: Icon(
-                              Icons.attachment,
-                              color: kPrimaryColor.withOpacity(0.85),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 5,
-                  ),
-                  CircleAvatar(
-                    backgroundColor: kPrimaryColor.withOpacity(0.5),
-                    child: Icon(
-                      Icons.mic,
-                      color: Colors.white,
-                    ),
-                  )
                 ],
               ),
             ),
-          )
-        ],
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                width: size.width,
+                decoration: BoxDecoration(
+                  color: kPrimaryColor.withOpacity(0.85),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: kPrimaryLightColor,
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.only(left: 10),
+                              child: Icon(
+                                Icons.emoji_emotions_rounded,
+                                color: kPrimaryColor.withOpacity(0.85),
+                              ),
+                            ),
+                            Expanded(
+                              child: TextField(
+                                controller: messageController,
+                                maxLines: null,
+                                decoration: InputDecoration(
+                                  contentPadding: EdgeInsets.symmetric(
+                                    horizontal: 10,
+                                  ),
+                                  border: InputBorder.none,
+                                  hintText: "Type here...",
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding: EdgeInsets.only(right: 10),
+                              child: Icon(
+                                Icons.attachment,
+                                color: kPrimaryColor.withOpacity(0.85),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    CircleAvatar(
+                      backgroundColor: kPrimaryColor.withOpacity(0.5),
+                      child: Icon(
+                        Icons.mic,
+                        color: Colors.white,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
       ),
-    ));
+    );
   }
 }
